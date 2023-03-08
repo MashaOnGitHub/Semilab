94f9a5b882bec1280c7dbcdc5978754a225188bf
Masha Baidachna <realcowcitizen@gmail.com>
2023-03-08 13:38:24 +0000

    discrete same endpoints

--- whitenoise.py
+++ whitenoise.py
@@ -1,9 +1,7 @@
 import numpy as np
-import scipy
 import matplotlib.pyplot as plt
 from scipy import signal
-from scipy.optimize import leastsq
-from scipy import linalg
+import math
 
 # WHITE NOISE
 # mean = 0
@@ -15,8 +13,8 @@ from scipy import linalg
 
 # Set the range and step of x manually
 start = 0
-stop = 6
-step = .2
+stop = 5
+step = .1
 
 mean = 0
 std = .5
@@ -25,19 +23,34 @@ y = np.random.normal(mean, std, size=num_samples)
 x = np.arange(start, stop, step)
 
 y_smooth = signal.savgol_filter(y, window_length=10, polyorder=3, mode="nearest")
-y_smooth2 = signal.savgol_filter(y_smooth, window_length=10, polyorder=3, mode = "nearest")
+y_smooth2 = signal.savgol_filter(y_smooth, window_length=15, polyorder=3, mode = "nearest")
 
 
-def estimateCurve():
+# Adjust ends to be the same max value
+def adjust_y(arr):
+    y_max = max(arr)
+    y_adjusted = arr
+    y_adjusted[0] = y_max
+    y_adjusted[-2] = y_max
+
+    # y_adjusted[1] = abs((y_max + y_adjusted[2])//2)
+    # y_adjusted[-3] = abs((y_max + y_adjusted[-4])//2)
+
+    # All points below 0
+    for i in range(len(y_adjusted)):
+        y_adjusted[i] = y_adjusted[i] - y_max
+
+    return y_adjusted
+
+
+def estimate_curve():
     matrix = []
     for i in range(num_samples-1):
         row = [1]
         for j in range(num_samples//2-1):
             row.append(np.sin((j+1)*x[i]))
             row.append(np.cos((j+1)*x[i]))
-        # row = [1, np.sin(x[i]), np.cos(x[i]), np.sin(2*x[i]), np.cos(2*x[i]), np.sin(3*x[i]), np.cos(3*x[i])]
         matrix.append(row)
-
 
     # Logging for shape
     # print(matrix)
@@ -46,48 +59,34 @@ def estimateCurve():
     # print("y smooth twice dimension:", len(y_smooth2[0:num_samples-1]))
     # print("Length of x:", len(x))
 
-    c = np.linalg.solve(matrix, y_smooth2[0:num_samples-1])
+    y_adjusted = adjust_y(y_smooth2)
+    c = np.linalg.solve(matrix, y_adjusted[:-1])
 
     # Print the function with constants
     strings = ["sin", "cos"]
     equation = [str(c[0]), "+"]
     for i in range(1, num_samples-1):
         equation.append(str(c[i]))
-        equation.append(strings[i%2])
+        equation.append(strings[(i+1)%2])
         equation.append(str((i+1)//2))
-        equation.append("x + ")
+        equation.append("x+")
     equation.pop()
+    equation.append("x")
     print("".join(equation))    
 
+    # Find the y values from the estimated curve
     a = np.array(matrix)
     est_y = a.dot(c)
    
     return est_y[:len(x)-1]
 
 
-# def estimateCurve():
-    matrix = []
-    # row = [1]
-    for i in range(7): # len(y_smooth2)
-        # row.append(np.sin((i+1)*x[i]))
-        # row.append(*np.cos((i+1)*x[i]))
-        row = [1, np.sin(x[i]), np.cos(x[i]), np.sin(2*x[i]), np.cos(2*x[i]), np.sin(3*x[i]), np.cos(3*x[i])]
-        matrix.append(row)
-
-    c = np.linalg.solve(matrix, y_smooth2[0:7])
-
-    print(str(c[0]) + " + " + str(c[1]) + " * sin(x)" + str(c[2]) + " * cos(x)" + str(c[3]) + " * sin(2x)" + str(c[4]) + " * cos(2x)" + str(c[5]) + " * sin(3x)" + str(c[6]) + " * cos(3x)")
-    est_y = []
-    for i in range(len(x)):
-        est_y.append(c[0] + c[1]*np.sin(x[i]) + c[2]*np.cos(x[i]) + c[3]*np.sin(2*x[i]) + c[4]*np.cos(2*x[i]) + c[5]*np.sin(3*x[i]) + c[6]*np.cos(3*x[i]))
-
-    return est_y[0:len(x)-3]
-
 
 plt.figure(figsize=(12, 4))
 plt.plot(x, y, label = "original")
 plt.plot(x, y_smooth, label="y_smoothed_once")
 plt.plot(x, y_smooth2, linewidth=3, label="y_smoothed_twice")
+plt.plot(x, adjust_y(y_smooth2), linewidth=4, label="y_adjusted")
+plt.plot(x[:len(x)-1], estimate_curve(), label="estimated function")
 plt.legend()
-plt.plot(x[:len(x)-1], estimateCurve(), label="estimated function")
 plt.show()
